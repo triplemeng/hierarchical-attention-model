23492ca10451dc95f7a9b542e09c3860afcdd3cc
mmeng <mmeng@linkedin.com>
2017-10-21 09:22:04 +0800

    modulize visualization

--- code/components.py
+++ code/components.py
@@ -1,3 +1,4 @@
+import os, re
 import tensorflow as tf
 import numpy as np
 
@@ -76,6 +77,45 @@ def attention(atten_inputs, atten_size):
     atten_outs = tf.reduce_sum(atten_inputs * tf.reshape(alphas, [-1, max_time, 1]), 1)
     print("atten outs: "+str(atten_outs))
     return atten_outs, alphas
+
+def visualize_sentence_format(sent):
+    ## remove the trailing 'STOP' symbols from sent
+    visual_sent = ' '.join(re.sub('STOP', '', sent).split()) 
+    return visual_sent
+
+def visualize(sess, inputs, revlens, max_rev_length, keep_probs, index2word, alphas_words, alphas_sents,  x_test, y_test, y_predict, visual_sample_index):
+    visual_dir = "../visualization"
+    # visualization
+    sents_visual_file = os.path.join(visual_dir, "sents_in_review_visualization_{}.html".format(visual_sample_index))
+    x_test_sample = x_test[visual_sample_index:visual_sample_index+1]
+    y_test_sample = y_test[visual_sample_index:visual_sample_index+1]
+    test_dict = {inputs:x_test_sample, revlens: [max_rev_length], keep_probs: [1.0, 1.0]}
+    alphas_words_test, alphas_sents_test = sess.run([alphas_words, alphas_sents], feed_dict=test_dict) 
+    y_test_predict = sess.run(y_predict, feed_dict=test_dict)
+    print("test sample is {}".format(y_test_sample[0]))
+    print("test sample is predicted as {}".format(y_test_predict[0]))
+    print(alphas_words_test.shape)
+
+    # visualize a review
+    sents = [get_sentence(index2word, x_test_sample[0][i]) for i in range(max_rev_length)]
+    index_sent = 0
+    print("sents size is {}".format(len(sents)))
+    with open(sents_visual_file, "w") as html_file:
+        html_file.write('actual label: %f, predicted label: %f<br>' % (y_test_sample[0], y_test_predict[0]))
+        for sent, alpha in zip(sents, alphas_sents_test[0] / alphas_sents_test[0].max()):
+            if len(set(sent.split(' '))) == 1:
+                index_sent += 1
+                continue  
+            visual_sent = visualize_sentence_format(sent)
+            # display each sent's importance by color
+            html_file.write('<font style="background: rgba(255, 0, 0, %f)">&nbsp&nbsp&nbsp&nbsp&nbsp</font>' % (alpha))
+            visual_words = visual_sent.split()
+            visual_words_alphas = alphas_words_test[index_sent][:len(visual_words)]
+            # for each sent, display its word importance by color
+            for word, alpha_w in zip(visual_words, visual_words_alphas / visual_words_alphas.max()):
+                html_file.write('<font style="background: rgba(255, 255, 0, %f)">%s </font>' % (alpha_w, word))
+            html_file.write('<br>')
+            index_sent += 1
 
 if __name__ == '__main__':
     pass 
--- code/models.py
+++ code/models.py
@@ -5,7 +5,7 @@ import numpy as np
 import pickle as pl 
 import pandas as pd
 import tensorflow as tf
-from components import sequence, attention, BucketedDataIterator, get_sentence
+from components import sequence, attention, BucketedDataIterator, get_sentence, visualize_sentence_format, visualize
 
 def build_graph(
     inputs,
@@ -69,10 +69,6 @@ def build_graph(
     
     return dense, alphas_words, alphas_sents
 
-def visualize_sentence(sent):
-    ## remove the trailing 'STOP' symbols from sent
-    visual_sent = ' '.join(re.sub('STOP', '', sent).split()) 
-    return visual_sent
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Parameters for building the model.')
@@ -89,7 +85,6 @@ if __name__=="__main__":
     epochs = args.epochs
 
     working_dir = "../data/aclImdb"
-    visual_dir = "../visualization"
     log_dir = "../logs"
     train_filename = os.path.join(working_dir, "train_df_file")
     test_filename = os.path.join(working_dir, "test_df_file")
@@ -166,7 +161,8 @@ if __name__=="__main__":
         total_batch2 = int(len(x_test)/(test_batch_size))
         avg_accu = 0.0
 
-        for i in range(total_batch2):
+        #for i in range(total_batch2):
+        for i in range(0):
             batch_x = x_test[i*test_batch_size:(i+1)*test_batch_size]
             batch_y = y_test[i*test_batch_size:(i+1)*test_batch_size]
             batch_seqlen = test_review_lens[i*test_batch_size:(i+1)*test_batch_size]
@@ -179,36 +175,5 @@ if __name__=="__main__":
             avg_accu += accu/total_batch2
 
         print("prediction accuracy on test set is {}".format(avg_accu))
-
-        # visualization
-        visual_sample_index = 100
-        sents_visual_file = os.path.join(visual_dir, "sents_in_review_visualization_{}.html".format(visual_sample_index))
-        x_test_sample = x_test[visual_sample_index:visual_sample_index+1]
-        y_test_sample = y_test[visual_sample_index:visual_sample_index+1]
-        test_dict = {inputs:x_test_sample, revlens: [max_rev_length], keep_probs: [1.0, 1.0]}
-        alphas_words_test, alphas_sents_test = sess.run([alphas_words, alphas_sents], feed_dict=test_dict) 
-        y_test_predict = sess.run(y_predict, feed_dict=test_dict)
-        print("test sample is {}".format(y_test_sample[0]))
-        print("test sample is predicted as {}".format(y_test_predict[0]))
-        print(alphas_words_test.shape)
-
-        # visualize a review
-        sents = [get_sentence(index2word, x_test_sample[0][i]) for i in range(max_rev_length)]
-        index_sent = 0
-        print("sents size is {}".format(len(sents)))
-        with open(sents_visual_file, "w") as html_file:
-            html_file.write('actual label: %f, predicted label: %f<br>' % (y_test_sample[0], y_test_predict[0]))
-            for sent, alpha in zip(sents, alphas_sents_test[0] / alphas_sents_test[0].max()):
-                if len(set(sent.split(' '))) == 1:
-                    index_sent += 1
-                    continue  
-                visual_sent = visualize_sentence(sent)
-                # display each sent's importance by color
-                html_file.write('<font style="background: rgba(255, 0, 0, %f)">&nbsp&nbsp&nbsp&nbsp&nbsp</font>' % (alpha))
-                visual_words = visual_sent.split()
-                visual_words_alphas = alphas_words_test[index_sent][:len(visual_words)]
-                # for each sent, display its word importance by color
-                for word, alpha_w in zip(visual_words, visual_words_alphas / visual_words_alphas.max()):
-                    html_file.write('<font style="background: rgba(255, 255, 0, %f)">%s </font>' % (alpha_w, word))
-                html_file.write('<br>')
-                index_sent += 1
+        visual_sample_index = 99
+        visualize(sess, inputs, revlens, max_rev_length, keep_probs, index2word, alphas_words, alphas_sents,  x_test, y_test, y_predict, visual_sample_index)
